e9f3745da94356c1d1e51e531d87c35fecadf1c1
joshdewhurst <joshuardewhurst@gmail.com>
2022-08-25 15:42:52 -0700

    commit x

--- JS/main.js
+++ JS/main.js
@@ -26,7 +26,7 @@ render () {
 
 }
 
-const hermie = new GamePiece(10, 10, 50, 50, "pink")
+const hermie = new GamePiece(10, 10, 20, 20, "pink")
 const crab = new GamePiece(500, 250, 80, 80, "red")
 const sandcastle = new GamePiece(this.x, this.y, 60, 60, 'brown')
 const shell = new GamePiece(this.x, this.y, 100, 100, 'blue')
@@ -87,12 +87,13 @@ class Crab {
         ctx.closePath();
         ctx.stroke();
     }
+
+
 }
 
 function addCrab () {
     if (gameFrame % 400 === 0) {
         crabArray.push(new Crab());
-        console.log(crabArray.length)
     }
     for (i=0; i < crabArray.length; i++) {
         crabArray[i].update()
@@ -127,7 +128,6 @@ class SandCastle {
 function addSand () {
     if (gameFrame % 300 === 0) {
         sandArray.push(new SandCastle());
-        console.log(sandArray.length)
     }
     for (i=0; i < sandArray.length; i++) {
         sandArray[i].update()
@@ -135,16 +135,78 @@ function addSand () {
     }
 }
 
+// Create shell/end game
+const shellArray = []
+class Shell {
+    constructor() {
+        this.y = Math.random() * canvas.height;
+        this.x = canvas.width - 100;
+        this.radius = 80
+        this.speed = Math.random() * 1 + 1;
+        this.distance
+    }
+    update () {
+        this.x -= this.speed
+    }
+    draw () {
+        ctx.fillStyle = 'blue'
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+function addShell () {
+    if (gameFrame % 2000 === 0) {
+        shellArray.push(new Shell());
+    }
+    for (i=0; i < shellArray.length; i++) {
+        shellArray[i].update()
+        shellArray[i].draw()
+    }
+}
+
 // detect hit function
 
 
+// run clock
+let time = 0
+function runTimer () {
+    time ++
+    document.getElementById('timer').innerText = time
+    }
+    
+setInterval(runTimer, 1000)
+
+// detect hit
+function detectHit(objectOne, objectTwo) {
+    const left = objectOne.x + objectOne.width >= objectTwo.x
+    const right = objectOne.x <= objectTwo.x + objectTwo.width
+    const top = objectOne.y + objectOne.height >= objectTwo.y
+    const bottom = objectOne.y <= objectTwo.y + objectTwo.height
+    if (left && right && top && bottom) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// run game
 function runGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     addCrab();
     addSand();
+    if (time === 160) {
+        addShell()
+    }
     if (hermie.alive) {
         hermie.render()
     };
+    if (detectHit(hermie, crab)) {
+        console.log("boom")
+    }
     gameFrame++;
     requestAnimationFrame(runGame)
 }
